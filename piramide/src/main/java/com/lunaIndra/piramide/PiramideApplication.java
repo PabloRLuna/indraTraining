be449270644b43026c067eea9ad6c16e21042d4d
PabloRLuna <pabloluna.tyko@gmail.com>
2024-11-07 11:15:20 +0100

    initializr commit

---
+++ piramide/src/main/java/com/lunaIndra/piramide/PiramideApplication.java
@@ -0,0 +1,13 @@
+package com.lunaIndra.piramide;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PiramideApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PiramideApplication.class, args);
+	}
+
+}
---
+++ piramide/src/test/java/com/lunaIndra/piramide/PiramideApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lunaIndra.piramide;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PiramideApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
